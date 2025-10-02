e9190108df5cb5e7cd13a9f3c0bde8654393b092
salma akg <salmaakagou1@gmail.com>
2025-10-02 15:09:54 +0100

    initial commit

---
+++ helloCounter/app/src/main/java/com/solicode/hellocounter/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.solicode.hellocounter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { App() }
+    }
+}
+
+@Composable
+fun App() {
+    MaterialTheme {
+        Greeting()
+    }
+}
+
+@Composable
+fun Greeting() {
+    var count by remember { mutableStateOf(0) }
+
+    // Box = conteneur qui permet d'aligner son contenu
+    Box(
+        modifier = Modifier.fillMaxSize(),              // occupe tout l'écran
+        contentAlignment = Alignment.Center             // centre le contenu
+    ) {
+        Button(onClick = { count++ }) {
+            Text(text = stringResource(R.string.btn_hello))
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun PreviewApp() {
+    App()
+}
