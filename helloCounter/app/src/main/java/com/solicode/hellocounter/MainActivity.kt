58bbbb5e0358f37a14058b408735d0f85dd1cd30
salma akg <salmaakagou1@gmail.com>
2025-10-03 09:30:09 +0100

    ajout Finitions

--- helloCounter/app/src/main/java/com/solicode/hellocounter/MainActivity.kt
+++ helloCounter/app/src/main/java/com/solicode/hellocounter/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.solicode.hellocounter.ui.MainScreen
+import com.solicode.hellocounter.ui.theme.HelloCounterTheme
 import androidx.compose.ui.tooling.preview.Preview
 
 
@@ -19,7 +20,7 @@ import androidx.compose.ui.tooling.preview.Preview
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { MaterialTheme { MainScreen() } }
+        setContent { HelloCounterTheme { MainScreen() } }
     }
 }
 
